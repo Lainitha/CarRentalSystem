ef94cbdcdef873ecb0573dd5da65f10984f97ee8
Lainitha Krishnakumar <krishnakumarlainitha68@gmail.com>
2025-08-13 15:43:17 +0530

    Add new client class

---
+++ carrentalsystem/model/Client.java
@@ -0,0 +1,22 @@
+package carrentalsystem.model;
+import java.util.Scanner;
+
+public class Client extends User {
+
+    public Client(){
+        super();
+    }
+
+
+    @Override
+    public void showList(Database database, Scanner s) {
+        System.out.println("\n1. View Cars");
+        System.out.println("2. Rent Car");
+        System.out.println("3. Return Car");
+        System.out.println("4. Show My Rents");
+        System.out.println("5. Edit My Data");
+        System.out.println("6. Quits\n");
+
+    }
+    
+}
